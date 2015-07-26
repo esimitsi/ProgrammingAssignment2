3b57d183f4c6ec37110279b67c4f7d91eb46d1e0
esimitsi <E.Symitsi@uea.ac.uk>
2015-07-26 18:58:31 +0100

    Assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -11,5 +11,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ##Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data.")
+    return(inverse)
+  }
+  inf <- x$get()
+  inverse <- solve(inf)
+  x$setinverse(inverse)
+  inverse
 }
